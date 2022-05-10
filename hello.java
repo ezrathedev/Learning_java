ea5912ca7c7f4f553767382cadc9f1f6201ab96c
Ezra Hampton <ezra.hamptoniii@gmail.com>
2022-05-10 15:48:34 -0400

    learning java

---
+++ hello.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+
+
+// hide and seek using the diffrence between print and println
+public class HideAndSeek {
+  public static void main(String[] args) {
+    System.out.println("Let's play hide and seek.");
+
+System.out.print("Three...");
+System.out.print("Two...");
+System.out.println("One...");
+System.out.println("Ready or not, here I come!");
+
+  }
+}
